8876ee54969a8eaaaece2345d1035f9067780106
Charles Zivko <charleszivko@gmail.com>
2017-07-05 16:35:29 -0700

    i did a comment

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+// A comment
 namespace WebApplication1
 {
     public class MvcApplication : System.Web.HttpApplication
